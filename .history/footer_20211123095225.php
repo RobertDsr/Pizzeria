69374ce4f054578ed220bbfd4f43a35667d47518
Robert Dsr <szumdr@gmail.com>
2021-11-23 10:07:30 +0100

    footer

--- .history/footer_20211123095225.php
+++ .history/footer_20211123095225.php
@@ -12,8 +12,21 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://robertdreszer.pl/', 'thissite' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'thissite' ), 'RobertDreszer' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'thissite' ), 'thissite', '<a href="http://robertdreszer.pl">Robert Dreszer</a>' );
+				?>
+		</div><!-- .site-info -->
 
-	<div class="logo logoFooter">
+		<div class="logo logoFooter">
                 <img src="img/logo.png" alt="logo pizzerii">
             </div>
             <div class="address">
@@ -45,26 +58,10 @@
                 <p>@ Robert Dreszer</p>
             </div>
 
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://robertdreszer.pl/', 'thissite' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'thissite' ), 'RobertDreszer' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'thissite' ), 'thissite', '<a href="http://robertdreszer.pl">Robert Dreszer</a>' );
-				?>
-		</div><!-- .site-info -->
-
-
-
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
-<?php wp_footer('content-footer'); ?>
+<?php wp_footer('content'); ?>
 
 </body>
 </html>
