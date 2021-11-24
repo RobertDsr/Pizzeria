44f4ceb774a4bc1cb036b0075d7c722cd9b240fd
Robert Dsr <szumdr@gmail.com>
2021-11-24 19:08:04 +0100

    RWD start

---
+++ .history/single_20211123183528.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package thisSite
+ */
+
+get_header();
+?>
+
+<div class="ts-wrapper-posts">
+
+	<main id="primary" class="site-posts-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+
+			the_post_navigation(
+				array(
+					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Poprzedni:', 'thissite' ) . '</span> <span class="nav-title">%title</span>',
+					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'thissite' ) . '</span> <span class="nav-title">%title</span>',
+				)
+			);
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+?>
+</div>
+<?php
+get_footer('content');
