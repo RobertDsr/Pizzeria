74e87e7cb5db491d04857270fbf7ce9f34840fb3
Robert Dsr <szumdr@gmail.com>
2021-12-01 17:49:50 +0100

    blog page start

---
+++ .history/sidebar-post_20211201170258.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package thisSite
+ */
+
+
+if ( ! is_active_sidebar( 'sidebar-2' ) ) {
+	return;
+}
+?>
+
+<aside id="postSidebar" class="widget-area-postSidebar">
+	<?php dynamic_sidebar( 'sidebar-2' ); ?>
+</aside><!-- #secondary -->
---
+++ .history/sidebar_20211201165200.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package thisSite
+ */
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
+?>
+
+<aside id="secondary" class="widget-area">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</aside><!-- #secondary -->
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
+?>
+
+<aside id="secondary" class="widget-area">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</aside><!-- #secondary -->
