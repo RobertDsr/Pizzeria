df25220eea38c68aeb64de6f665e10902bc7fe87
Robert Dsr <szumdr@gmail.com>
2022-01-21 12:55:59 +0100

    end css main catalog, product catalog

---
+++ .history/sidebar-products_20220120070158.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package thisSite
+ */
+
+
+if ( ! is_active_sidebar( 'sidebar-3' ) ) {
+	return;
+}
+?>
+
+<aside id="productSidebar" class="widget-area-productSidebar">
+	<?php dynamic_sidebar( 'sidebar-2' ); ?>
+</aside><!-- #secondary -->
