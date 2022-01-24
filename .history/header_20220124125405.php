0cbaf35ede551abc8716cbac12feb8b65eede607
Robert Dsr <szumdr@gmail.com>
2022-01-24 15:43:29 +0100

    fixes in css menu

--- .history/header_20220124125405.php
+++ .history/header_20220124125405.php
@@ -65,7 +65,7 @@
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Zobacz Menu', 'thissite' ); ?></button>
 			<?php
-			// the_custom_logo();
+			the_custom_logo();
 			wp_nav_menu(
 				array(
 					'theme_location' => 'menu-1',
