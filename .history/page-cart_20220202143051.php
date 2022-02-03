40794dc5d96d3381032a3f9cda0d58ded5191e32
Robert Dsr <szumdr@gmail.com>
2022-02-03 15:51:43 +0100

    start rwd

---
+++ .history/page-cart_20220202143051.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: Strona koszyka
+ * This template displays a shopping cart page. Please be careful when modifying this file.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package thisSite
+ */
+
+get_header();
+?>
+<div class="ts-wrapper-cart">
+	<main id="primary" class="site-main">
+		
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+?>
+</div>
+<?php
+get_footer('content');
---
+++ .history/page-cart_20220202145557.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Shopping cart page
+ * 
+ * This template displays a shopping cart page.
+ * Please be careful when modifying this file.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package thisSite
+ */
+
+get_header();
+?>
+<div class="ts-wrapper-cart">
+	<main id="primary" class="site-main">
+		
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'cart' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar('');
+?>
+</div>
+<?php
+get_footer('content');
