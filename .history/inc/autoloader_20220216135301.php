6b01db866e86c98120be8077b0c973df92cf17b5
Robert Dsr <szumdr@gmail.com>
2022-02-16 13:56:04 +0100

    fix widget

--- .history/inc/autoloader_20220216135301.php
+++ .history/inc/autoloader_20220216135301.php
@@ -13,7 +13,7 @@ function thissite_autoloader($class) {
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
 
     $directory = get_template_directory();
-    $path = $directory . DIRECTORY_SEPARATOR . 'inc' . $class . '.php';
+    $path = $directory . DIRECTORY_SEPARATOR . 'inc'  . $class . '.php';
 
     if(file_exists($path)) {
         require_once($path);
