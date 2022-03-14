2378656799f3024bc4bd884edcb5d6cc46bf2847
Robert Dsr <szumdr@gmail.com>
2022-03-14 10:10:48 +0100

    rwd frontpage, subpages, footer

---
+++ .history/product-adder_20220307155451.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying all  product pages
+ *
+ * Please be careful when modifying this file.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package thisSite
+ */
+
+get_header();
+?>
+<div class="ts-wrapper-products">
+	<main id="primary" class="site-main=">
+
+		<?php
+		content_product_adder();
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar('products');
+?>
+</div>
+<?php
+get_footer('main');
---
+++ .history/template-parts/content-blog_20220307144228.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package thissite
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header-posts">
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title-posts">', '</h1>' );
+		else :
+			the_title( '<h1 class="entry-title-posts"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+		endif;
+
+		if ( 'post' === get_post_type() ) :
+			?>
+			<div class="entry-meta-posta">
+				<?php
+				thissite_posted_on();
+				thissite_posted_by();
+				?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+
+	<div class="entry-content">
+    <?php thissite_post_thumbnail(); ?>
+		<?php
+		the_excerpt(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'thissite' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				wp_kses_post( get_the_title() )
+			)
+		);
+
+        the_shortlink('Czytaj więcej', 'Przejdź do strony wpisu', '<span>', '</span>');
+
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'thissite' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php thissite_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
