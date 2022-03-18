cf6216e400ab158b6a02b74ac592a2f56c251eaa
Robert Dsr <szumdr@gmail.com>
2022-03-18 12:18:42 +0100

    Css fixes, refactoring

---
+++ .history/front-page_20220315153628.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying front page
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package thisSite
+ */
+
+get_header('front');
+?>
+<div class="ts-wrapper-front">
+	<main id="primary" class="site-main-front-page">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'frontPage' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+</div>
+<?php
+get_footer('main');
